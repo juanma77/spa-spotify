0f5ddfa46841d724bb1dcae87e6e2953dd0818b1
Juan Manuel Lopez Santana <freakshield777@hotmail.com>
2020-06-06 10:46:31 -0500

    passing params to artist component

--- src/app/components/cards/cards.component.ts
+++ src/app/components/cards/cards.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-cards',
@@ -9,9 +10,27 @@ export class CardsComponent implements OnInit {
 
   @Input() items: any[] = []; 
 
-  constructor() { }
+  constructor(private router: Router) {
+
+  }
 
   ngOnInit() {
+  }
+
+  public showArtist(item: any){
+
+    let artistId; 
+
+    // Para verificar que tenemos el id del artista tanto para la página de home como de search
+    if( item.type === 'artist' ){
+      artistId = item.id;
+    }else{
+      artistId = item.artists[0].id;
+    }
+
+    // Navegamos a la página de /artist y mandamos el artistId; allá en artist.component debemos de recibir el artistId
+    this.router.navigate([ '/artist', artistId ]);
+
   }
 
 }
